a95b52ece92b64018057bedc0965bfd174eaac76
Khaled-Youssef <khalid.yousif@gmail.com>
2024-04-06 17:04:19 +0200

    Final Result

---
+++ index.js
@@ -0,0 +1,37 @@
+const backgroundColorInput = document.getElementById('bgColor');
+backgroundColorInput.addEventListener("change", changeBgColor)
+function changeBgColor(){
+    document.querySelector(':root').style.setProperty ('--bgColor', backgroundColorInput.value)
+}
+
+const inputs = document.querySelectorAll("input")
+inputs.forEach(input=>input.addEventListener("change", handleUpdate))
+inputs.forEach(input=>input.addEventListener("mousemove", handleUpdate))
+
+inputs.forEach(input => {
+    getInitValues(input)
+});
+
+function getInitValues(inputName){
+    const input = document.querySelector(`input[name="${inputName.name}"]`)
+    const valueSpan = document.querySelector(`.highlighted.${inputName.name} .value`)
+    const inputValue = input.value
+    valueSpan.innerHTML = inputValue
+    document.querySelector(':root').style.setProperty(`--${inputName.name}`, inputName.value)
+}
+
+
+function handleUpdate(){
+    const input = document.querySelector(`input[name="${this.name}"]`)
+    const valueSpan = document.querySelector(`.highlighted.${this.name} .value`)
+    const inputValue = input.value
+    valueSpan.innerHTML = inputValue
+    document.querySelector(':root').style.setProperty(`--${input.name}`, inputValue)
+    console.log(input)
+    console.log(inputValue)
+}
+
+
+const copyrightsDiv = document.createElement("div")
+copyrightsDiv.innerHTML +=   '<div class="copyrights">Made by <a href="https://github.com/Khaled-Youssef/" target=\"_blank\"><img src="img/joe-dl-logo-w.png" alt="Khaled Youssef" srcset=""></a></div>'
+document.querySelector('body').appendChild(copyrightsDiv, 'beforeend')
